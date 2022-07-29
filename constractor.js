ca81485f3f8dc4f51da2f139b0f6b5f1171e81fa
prashanta0234 <prashanta0234@gmail.com>
2022-07-29 12:46:06 +0600

    update constructor

--- constractor.js
+++ constractor.js
@@ -6,9 +6,15 @@ const Computer = function (name, model, ram, storage, storage_type) {
   this.Ram = ram;
   this.Storage = storage;
   this.Storage_type = storage_type;
+
+  this.aboutPc = () => {
+    console.log(`Our laptop model number is${model} and name is${name}`);
+  };
 };
 
 const dell = new Computer("DEll", "m256", "4gb", "250gb", "SSD");
 const hp = new Computer("HP", "hp666", "8gb", "1TB", "HDD");
 console.log(dell, hp);
 console.log(`Our dell laptop model number is${dell.Model}`);
+dell.aboutPc();
+hp.aboutPc();
